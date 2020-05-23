ac3e0536beb4310bd53b6943c72d6199a56c18bc
Farman khan <farman.khan@inmobi.com>
2020-05-23 16:26:50 +0530

    initial commit

---
+++ app/src/main/java/com/demo/kotlinmvvm/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.demo.kotlinmvvm.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.kotlinmvvm.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/demo/kotlinmvvm/auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.demo.kotlinmvvm.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.demo.kotlinmvvm.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
